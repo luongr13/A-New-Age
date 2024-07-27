28558895f0bc97073fc057aac872465f3d4f089a
luongr13 <50896340+luongr13@users.noreply.github.com>
2024-07-27 18:35:39 -0500

    Fix attribute not setting for toggle

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ toggle.addEventListener('click', () => {
   if (nav_menu.classList.contains('hidden')) {
     nav_menu.classList.remove('hidden');
     toggle.textContent = 'Close';
-    this.setAttribute('aria-expanded', 'true');
+    toggle.setAttribute('aria-expanded', 'true');
   } else {
     nav_menu.classList.add('hidden');
     toggle.textContent = 'Menu';
-    this.setAttribute('aria-expanded', 'false');
+    toggle.setAttribute('aria-expanded', 'false');
   }
 })

f81280ae6c8c0f006acab8b5ff18686cf49f89ba
luongr13 <50896340+luongr13@users.noreply.github.com>
2024-07-27 22:33:48 -0500

    Complete makeover

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-let nav_menu = document.querySelector('#menu');
+let nav_menu = document.querySelector('.menu-container');
 let toggle = document.querySelector('#toggle');
 
 toggle.addEventListener('click', () => {
-  if (nav_menu.classList.contains('hidden')) {
-    nav_menu.classList.remove('hidden');
-    toggle.textContent = 'Close';
+  if (nav_menu.classList.contains('not-active')) {
+    nav_menu.classList.remove('not-active');
     toggle.setAttribute('aria-expanded', 'true');
   } else {
-    nav_menu.classList.add('hidden');
-    toggle.textContent = 'Menu';
+    nav_menu.classList.add('not-active');
     toggle.setAttribute('aria-expanded', 'false');
   }
 })
